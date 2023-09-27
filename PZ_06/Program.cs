a6ad921513bf0af8b1b65194cd91d31b940ebf28
ADMIN <ADMIN@DESKTOP-0R1O7Q0>
2023-09-27 14:11:25 +0300

    Добавьте файлы проекта.

---
+++ PZ_06/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PZ_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(); // заполнить массив из 10 случайныз числел в деапазоне от -10..10.
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };// случайные числа от -10 до 10
+            int k = 9; // сдвиг, значеине переменной k
+            int tmt;
+            int nextInd = 0;
+            Console.WriteLine(string.Join(" ", arr));
+            for (int i = 1;  i < arr.Length - 0; i++) 
+            {
+                nextInd += k;
+                nextInd %= arr.Length;
+
+                tmt = arr[nextInd];
+                arr[nextInd] = arr[0];
+                arr[0] = tmt;
+            }
+            Console.WriteLine(string.Join(' ', arr)); // вывод результата на экран
+
+
+
+        }
+    }
+}
